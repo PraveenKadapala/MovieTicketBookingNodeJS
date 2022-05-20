c65582c3fd027aaa1de5daabcd1293f235729009
PraveenKadapala <praveenkadapala@gmail.com>
2022-05-20 14:49:42 +0530

    Updated

--- middlewares/passport.js
+++ middlewares/passport.js
@@ -1,12 +1,10 @@
 const JwtStrategy = require('passport-jwt').Strategy
 const ExtractJWT = require('passport-jwt').ExtractJwt
-const opts={}
 const passport = require('passport')
 const User = require("../models/usermodel");
 
-
 opts.jwtFromRequest = ExtractJWT.fromAuthHeaderAsBearerToken()
-opts.secretOrKey = "jadgfahbnab%dnalhfl#abf%jl@abljf"
+ 
 passport.use(new JwtStrategy(opts, function(jwt_payload,done){
     console.log(jwt_payload)
     User.findOne({id:jwt_payload.id},function(err,user){
